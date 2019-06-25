fde391856e1680bd44f3582d4cb5d205fbf0d7b0
Luiz Fernando Silva <luiz.silva@concrete.com.br>
2019-06-25 20:36:51 -0300

    Extracting dependency checker code into a separate class

--- Sources/TargetDependencyCheckerLib/Checker.swift
+++ Sources/TargetDependencyCheckerLib/Checker.swift
@@ -41,130 +41,14 @@ public enum Checker {
         
         let packageManager = try packageDiscovery.packageManager()
         
-        var visitedImports: Set<ImportVisit> = []
-        
-        let inspections =
-            try collectInspectionTargets(
-                packageManager: packageManager,
-                includePattern: options.includePattern,
-                excludePattern: options.excludePattern)
-        
-        for inspection in inspections {
-            try inspect(inspection: inspection,
-                        packageManager: packageManager,
-                        options: options,
-                        visitedImports: &visitedImports)
-        }
-    }
-    
-    static func collectInspectionTargets(packageManager: PackageManager,
-                                         includePattern: String?,
-                                         excludePattern: String?) throws -> [ImportInspection] {
-        
         let fileManagerDelegate = DiskFileManagerDelegate()
         
-        var inspectionTargets: [ImportInspection] = []
+        let dependencyChecker =
+            DependencyChecker(options: options,
+                              packageManager: packageManager,
+                              fileManagerDelegate: fileManagerDelegate)
         
-        let operationQueue = OperationQueue()
-        let inspectionTargetsMutex = Mutex()
-        var error: Error?
-        let errorMutex = Mutex()
-        
-        for target in packageManager.targets {
-            let files =
-                try packageManager
-                    .sourceFiles(for: target,
-                                 includePattern: includePattern,
-                                 excludePattern: excludePattern)
-            
-            for file in files {
-                operationQueue.addOperation {
-                    let fileManager =
-                        SourceFileManager(sourceFile: file,
-                                          fileManagerDelegate: fileManagerDelegate)
-                    
-                    do {
-                        let importedFrameworkDeclarations = try fileManager.importedFrameworks()
-                        
-                        let inspection =
-                            ImportInspection(file: file,
-                                             target: target,
-                                             importedFrameworks: importedFrameworkDeclarations)
-                        
-                        inspectionTargetsMutex.locking {
-                            inspectionTargets.append(inspection)
-                        }
-                    } catch let e {
-                        errorMutex.locking {
-                            error = e
-                        }
-                    }
-                }
-            }
-        }
-        
-        operationQueue.waitUntilAllOperationsAreFinished()
-        
-        if let error = error {
-            throw error
-        }
-        
-        // Sort files by path to result in a predictable diagnostical output
-        return inspectionTargets.sorted(by: { $0.file.path.path < $1.file.path.path })
-    }
-    
-    static func inspect(inspection: ImportInspection,
-                        packageManager: PackageManager,
-                        options: Options,
-                        visitedImports: inout Set<ImportVisit>) throws {
-        
-        let file = inspection.file
-        let target = inspection.target
-        let importedFrameworkDeclarations = inspection.importedFrameworks
-        
-        let diagnosticsTarget = options.outputType.diagnosticsOutput
-        
-        let rootPath = packageManager.packageRootUrl.path
-        let dependencyGraph = try packageManager.dependencyGraph()
-        
-        let relativePath =
-            String(file.path.path.replacingOccurrences(of: rootPath, with: "").drop(while: { $0 == "/" }))
-        
-        for importDecl in importedFrameworkDeclarations {
-            if options.warnOncePerFramework {
-                let importVisit = ImportVisit(framework: importDecl.frameworkName, target: target)
-                if !visitedImports.insert(importVisit).inserted {
-                    continue
-                }
-            }
-            
-            if !dependencyGraph.hasPath(from: importDecl.frameworkName, to: target.name) {
-                diagnosticsTarget
-                    .reportNonDependencyImport(
-                        importDecl: importDecl,
-                        target: target,
-                        file: file,
-                        relativePath: relativePath)
-            } else if options.warnIndirectDependencies && !dependencyGraph.hasEdge(from: importDecl.frameworkName, to: target.name) {
-                diagnosticsTarget
-                    .reportNonDirectDependencyImport(
-                        importDecl: importDecl,
-                        target: target,
-                        file: file,
-                        relativePath: relativePath)
-            }
-        }
-    }
-    
-    struct ImportInspection {
-        var file: SourceFile
-        var target: Target
-        var importedFrameworks: [ImportedFrameworkDeclaration]
-    }
-    
-    struct ImportVisit: Hashable {
-        var framework: String
-        var target: Target
+        try dependencyChecker.inspect()
     }
 }
 
---
+++ Sources/TargetDependencyCheckerLib/DependencyChecker.swift
@@ -0,0 +1,130 @@
+import Foundation
+
+class DependencyChecker {
+    let options: Checker.Options
+    let packageManager: PackageManager
+    let fileManagerDelegate: FileManagerDelegate
+    
+    init(options: Checker.Options, packageManager: PackageManager, fileManagerDelegate: FileManagerDelegate) {
+        self.options = options
+        self.packageManager = packageManager
+        self.fileManagerDelegate = fileManagerDelegate
+    }
+    
+    func inspect() throws {
+        var visitedImports: Set<ImportVisit> = []
+        
+        let inspections =
+            try collectInspectionTargets(includePattern: options.includePattern,
+                                         excludePattern: options.excludePattern)
+        
+        for inspection in inspections {
+            try inspect(inspection: inspection, visitedImports: &visitedImports)
+        }
+    }
+    
+    func collectInspectionTargets(includePattern: String?,
+                                  excludePattern: String?) throws -> [ImportInspection] {
+        
+        var inspectionTargets: [ImportInspection] = []
+        
+        let operationQueue = OperationQueue()
+        let inspectionTargetsMutex = Mutex()
+        var error: Error?
+        let errorMutex = Mutex()
+        
+        for target in packageManager.targets {
+            let files =
+                try packageManager
+                    .sourceFiles(for: target,
+                                 includePattern: includePattern,
+                                 excludePattern: excludePattern)
+            
+            for file in files {
+                operationQueue.addOperation { [fileManagerDelegate] in
+                    let fileManager =
+                        SourceFileManager(sourceFile: file,
+                                          fileManagerDelegate: fileManagerDelegate)
+                    
+                    do {
+                        let importedFrameworkDeclarations = try fileManager.importedFrameworks()
+                        
+                        let inspection =
+                            ImportInspection(file: file,
+                                             target: target,
+                                             importedFrameworks: importedFrameworkDeclarations)
+                        
+                        inspectionTargetsMutex.locking {
+                            inspectionTargets.append(inspection)
+                        }
+                    } catch let e {
+                        errorMutex.locking {
+                            error = e
+                        }
+                    }
+                }
+            }
+        }
+        
+        operationQueue.waitUntilAllOperationsAreFinished()
+        
+        if let error = error {
+            throw error
+        }
+        
+        // Sort files by path to result in a predictable diagnostical output
+        return inspectionTargets.sorted(by: { $0.file.path.path < $1.file.path.path })
+    }
+    
+    func inspect(inspection: ImportInspection,
+                 visitedImports: inout Set<ImportVisit>) throws {
+        
+        let file = inspection.file
+        let target = inspection.target
+        let importedFrameworkDeclarations = inspection.importedFrameworks
+        
+        let diagnosticsTarget = options.outputType.diagnosticsOutput
+        
+        let rootPath = packageManager.packageRootUrl.path
+        let dependencyGraph = try packageManager.dependencyGraph()
+        
+        let relativePath =
+            String(file.path.path.replacingOccurrences(of: rootPath, with: "").drop(while: { $0 == "/" }))
+        
+        for importDecl in importedFrameworkDeclarations {
+            if options.warnOncePerFramework {
+                let importVisit = ImportVisit(framework: importDecl.frameworkName, target: target)
+                if !visitedImports.insert(importVisit).inserted {
+                    continue
+                }
+            }
+            
+            if !dependencyGraph.hasPath(from: importDecl.frameworkName, to: target.name) {
+                diagnosticsTarget
+                    .reportNonDependencyImport(
+                        importDecl: importDecl,
+                        target: target,
+                        file: file,
+                        relativePath: relativePath)
+            } else if options.warnIndirectDependencies && !dependencyGraph.hasEdge(from: importDecl.frameworkName, to: target.name) {
+                diagnosticsTarget
+                    .reportNonDirectDependencyImport(
+                        importDecl: importDecl,
+                        target: target,
+                        file: file,
+                        relativePath: relativePath)
+            }
+        }
+    }
+    
+    struct ImportInspection {
+        var file: SourceFile
+        var target: Target
+        var importedFrameworks: [ImportedFrameworkDeclaration]
+    }
+    
+    struct ImportVisit: Hashable {
+        var framework: String
+        var target: Target
+    }
+}
