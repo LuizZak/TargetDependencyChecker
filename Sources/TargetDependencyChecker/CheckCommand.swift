429c4f6c09ac37304864009000aaabbf2217b979
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2020-02-28 10:20:42 -0300

    Adjusting whitespace

--- Sources/TargetDependencyChecker/CheckCommand.swift
+++ Sources/TargetDependencyChecker/CheckCommand.swift
@@ -15,29 +15,31 @@ struct CheckCommand: ParsableCommand {
             Defaults to 'terminal' if not provided.
             
                 terminal
-                    Prints output of conversion in a format proper for terminal's standard output.
+                    Prints output of conversion in a format proper for \
+            terminal's standard output.
                 
                 xcode
-                    Prints output with leading file/line numbers as warnings that Xcode can detect when used as a build phase.
+                    Prints output with leading file/line numbers as warnings \
+            that Xcode can detect when used as a build phase.
             
             """)
     var outputType: OutputType?
     
     @Flag(name: [.long, .customShort("o")], help: """
-            When specified, omits warnings of violations for frameworks \
-            that where already reported in previous files in the \
-            same target.
+            When specified, omits warnings of violations for frameworks that \
+            where already reported in previous files in the same target.
             """)
     var warnOncePerFramework: Bool
     
     @Flag(name: [.long, .customShort("i")], help: """
-            When specified, warns when importing a target that is not \
-            a direct dependency into another target.
+            When specified, warns when importing a target that is not a direct \
+            dependency into another target.
             """)
     var warnIndirectDependencies: Bool
     
     @Option(help: """
-            Ignores all includes in the string separated by commas provided to this argument.
+            Ignores all includes in the string separated by commas provided to \
+            this argument.
             """)
     var ignoreIncludes: String?
     
