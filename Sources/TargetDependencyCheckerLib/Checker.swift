b5d10a886d04160a9f96b1ff9fad20c8db73abff
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2020-02-28 21:30:07 -0300

    Adding doc comments

--- Sources/TargetDependencyCheckerLib/Checker.swift
+++ Sources/TargetDependencyCheckerLib/Checker.swift
@@ -11,12 +11,26 @@ public enum Checker {
     }
     
     public struct Options {
+        /// When specified, warns when importing a target that is not a direct
+        /// dependency into another target.
         public var warnIndirectDependencies: Bool
+        
+        /// When specified, omits warnings of violations for frameworks that
+        /// where already reported in previous files in the same target.
         public var warnOncePerFramework: Bool
+        
+        /// Specifies the path for the directory containing a Package.swift
+        /// manifest for a Swift Package Manager project.
         public var packageDirectory: URL?
+        
+        /// Specifies the format of the output.
         public var outputType: OutputType
+        
         public var includePattern: String?
         public var excludePattern: String?
+        
+        /// Ignores all includes in the string separated by commas provided to
+        /// this argument.
         public var ignoreIncludes: Set<String>
         
         public init(warnIndirectDependencies: Bool = false,
